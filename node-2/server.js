ff2ba2513afe62697b4872381cfc02ec4bf48fe9
Naphan Benchasattabuse <naphanben@gmail.com>
2017-05-12 16:23:06 +0700

    test travis

---
+++ node-2/server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const express = require('express');
+const NodeID = 2;
+// Constants
+const PORT = 8080;
+
+// App
+const app = express();
+app.get('/', function (req, res) {
+  res.send(`Hello world this is node-${NodeID}\n`);
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
