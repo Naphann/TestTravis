7150f9e04c8fc3287770f658d890b99e7d8f3ac6
Naphan Benchasattabuse <naphanben@gmail.com>
2017-05-12 17:26:35 +0700

    add timeout before test

--- client/client.js
+++ client/client.js
@@ -4,14 +4,18 @@ const request = require('request');
 // Constants
 const PORT = 8080;
 
-for (let instance = 1; instance <= 3; instance++) {
-  request(`http://node-${instance}:8080`, (err, resp, body) => {
+function test() {
+  for (let instance = 1; instance <= 3; instance++) {
+    request(`http://node-${instance}:8080`, (err, resp, body) => {
 
-    if (err) {
-      console.log(`error: ${err}`);
-      return;
-    }
-    console.log(`$instance-${instance} statusCode: ${resp.statusCode}`)
-    console.log(`$instance-${instance} body: ${body}`)
-  })
+      if (err) {
+        console.log(`error: ${err}`);
+        return;
+      }
+      console.log(`$instance-${instance} statusCode: ${resp.statusCode}`)
+      console.log(`$instance-${instance} body: ${body}`)
+    })
+  }
 }
+
+setTimeout(test, 5000);
